646aab507b2d20465bb0bda903eecbc9418e175a
levantinh20it288 <97578739+levantinh20it288@users.noreply.github.com>
2022-01-12 11:28:10 +0700

    Add files via upload

---
+++ THuc hanh(12/1)/app/src/main/java/com/example/thuchanh/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.thuchanh
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btncong.setOnClickListener {
+          var kq :Double = (so1.text.toString()).toDouble() + so2.text.toString().toDouble()
+            tvketqua.text = kq.toString()
+
+            btntru.setOnClickListener {
+                var kq :Double = (so1.text.toString()).toDouble() - so2.text.toString().toDouble()
+                tvketqua.text = kq.toString()
+
+                btnnhan.setOnClickListener {
+                    var kq :Double = (so1.text.toString()).toDouble() * so2.text.toString().toDouble()
+                    tvketqua.text = kq.toString()
+
+                    btnchia.setOnClickListener {
+                        var kq :Double = (so1.text.toString()).toDouble() / so2.text.toString().toDouble()
+                        tvketqua.text = kq.toString()
+        }
+
+    }
+}
